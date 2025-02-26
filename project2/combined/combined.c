b3c2acb0a1b4bbbc053fb41b86c4cf8b004ba2f9
Arabella Yao <yyao372@gatech.edu>
2025-02-26 12:29:57 -0500

    Create combined.c

---
+++ project2/combined/combined.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <stdbool.h>
+
+#include "combined.h"
+
+void combined_init(num_processes, num_threads) {
+
+}
+
+void combined_barrier() {
+
+}
+
+void combined_finalize() {}
