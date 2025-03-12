5ed37601403e4110082503e456024c4f18ef00f7
Yao, Yixing <yyao372@gatech.edu>
2025-03-12 19:09:24 -0400

    Added getProducts in store

--- project3/src/store.cc
+++ project3/src/store.cc
@@ -3,7 +3,12 @@
 #include <iostream>
 
 class store { 
+	Status getProducts(ServerContext* context, const ProductQuery* query, ProductReply* reply) override {
+		string product_name = query->product_name();
 
+		
+		return Status::OK;
+	}
 };
 
 int main(int argc, char** argv) {
